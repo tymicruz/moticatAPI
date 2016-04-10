6d2e9e7c037890a2c75a54214ea0e2b91b909f89
Tyler Cruz <tylercruz@Tylers-MacBook-Air.local>
2016-04-10 16:49:10 -0700

    adding something to make jay stop crying

--- server.js
+++ server.js
@@ -27,6 +27,16 @@ MongoClient.connect('mongodb://localhost:27017/moticat', function(err, db) {
 	app.use(bodyParser.urlencoded({extended: true}));
 	app.use(methodOverride('X-HTTP-Mthod-Override'));
 
+	////////
+	app.use(function(req, res, next){
+		res.header('Access-Control-Allow-Origin', "*");
+		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+		res.header('Access-Control-Allow-Headers', 'Content-Type');
+		next();
+	})
+	///////
+	
+
 	app.use(express.static(__dirname + '/public'));
 	app.use(express.static(__dirname + '/bower_components'));
 	
